4cef85c5d7424dfc499acafb2732d6f2fd8e651d
gregoryann <32854050+gregoryann@users.noreply.github.com>
2022-07-04 13:35:39 +0100

    feat: darkmode

--- src/App.js
+++ src/App.js
@@ -1,29 +1,33 @@
 import React, { useState, useEffect } from 'react';
 import './App.scss';
 import { firebase } from './firebase';
-import { Header } from './components/layout/Header';
-import { Content } from './components/layout/Content';
-
 import React, { useState } from 'react';
 import './App.scss';
 import { Header } from './components/layout/Header';
 import { Content } from './components/layout/Content';
 import { useProjects } from './hooks';
-
-
 export const App = () => {
     const { projects, setProjects } = useProjects();
     const [selectedProject, setSelectedProject] = useState('INBOX');
+    const [darkMode, setDarkMode] = useState(false);
 
     return (
-        <main data-testid="application">
-            <Header projects={projects} selectedProject={selectedProject} />
-            <Content
-                projects={projects}
-                setProjects={setProjects}
-                selectedProject={selectedProject}
-                setSelectedProject={setSelectedProject}
-            />
-        </main>
-    );
+           <main
+                data-testid="application"
+                className={darkMode ? 'darkmode' : undefined}
+            >
+                <Header
+                    projects={projects}
+                    selectedProject={selectedProject}
+                    darkMode={darkMode}
+                    setDarkMode={setDarkMode}
+                />
+                <Content
+                    projects={projects}
+                    setProjects={setProjects}
+                    selectedProject={selectedProject}
+                    setSelectedProject={setSelectedProject}
+                />
+            </main>
+            );
 };
--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -4,42 +4,49 @@ import React, { useState } from 'react';
 import { FaPizzaSlice } from 'react-icons/fa';
 import { AddTask } from '../AddTask';
 
-export const Header = ({ selectedProject, projects }) => {
-    const [shouldShowMain, setShouldShowMain] = useState(false);
-    const [showQuickAddTask, setShowQuickAddTask] = useState(false);
+export const Header = ({
+        selectedProject,
+        projects,
+        darkMode,
+        setDarkMode,
+    }) => {
+        const [shouldShowMain, setShouldShowMain] = useState(false);
+        const [showQuickAddTask, setShowQuickAddTask] = useState(false);
 
-    return (
-        <header className="header">
-            <nav>
-                <div className="logo">
-                    <img src="/images/logo.png" alt="Todoist" />
-                </div>
-                <div className="settings">
-                    <ul>
-                        <li
-                            className="settings__add"
-                            onClick={() => {
-                                setShowQuickAddTask(true);
-                                setShouldShowMain(true);
-                            }}
-                        >
-                            +
-                        </li>
-                        <li className="settings__dark-mode">
-                            <FaPizzaSlice />
-                        </li>
-                    </ul>
-                </div>
-            </nav>
-
-            <AddTask
-                selectedProject={selectedProject}
-                projects={projects}
-                showAddTaskMain={false}
-                shouldShowMain={shouldShowMain}
-                showQuickAddTask={showQuickAddTask}
-                setShowQuickAddTask={setShowQuickAddTask}
-            />
-        </header>
-    );
-};
+        return (
+            <header className="header">
+                <nav>
+                    <div className="logo">
+                        <img src="/images/logo.png" alt="Todoist" />
+                    </div>
+                    <div className="settings">
+                        <ul>
+                            <li
+                                className="settings__add"
+                                onClick={() => {
+                                    setShowQuickAddTask(true);
+                                    setShouldShowMain(true);
+                                }}
+                            >
+                                +
+                            </li>
+                                <li
+                                    className="settings__dark-mode"
+                                    onClick={() => setDarkMode(!darkMode)}
+                                >
+                                    <FaPizzaSlice />
+                                </li>
+                        </ul>
+                    </div>
+                </nav>
+                <AddTask
+                    selectedProject={selectedProject}
+                    projects={projects}
+                    showAddTaskMain={false}
+                    shouldShowMain={shouldShowMain}
+                    showQuickAddTask={showQuickAddTask}
+                    setShowQuickAddTask={setShowQuickAddTask}
+                />
+            </header>
+        );
+    };
