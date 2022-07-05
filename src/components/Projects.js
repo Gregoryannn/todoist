f2595f9db8b045423117b7fdfa0cd4420ab5f5c8
gregoryann <32854050+gregoryann@users.noreply.github.com>
2022-07-05 10:52:22 +0100

    feat: stateful project delete

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -2,38 +2,30 @@
 /* eslint-disable jsx-a11y/click-events-have-key-events */
 /* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
 import React, { useState } from 'react';
-import { FaTrashAlt } from 'react-icons/fa';
 import { useSelectedProjectValue, useProjectsValue } from '../context';
+import { IndividualProject } from './IndividualProject';
 
 export const Projects = () => {
-        const [active, setActive] = useState();
-        const { setSelectedProject } = useSelectedProjectValue();
-        const { projects } = useProjectsValue();
-
-        return (
-            projects &&
-            projects.map(project => (
-                <li
-                    key={project.projectId}
-                    className={
-                        active === project.projectId
-                            ? 'active sidebar__project'
-                            : 'sidebar__project'
-                    }
-                    onClick={() => {
-                        setActive(project.projectId);
-                        setSelectedProject(project.projectId);
-                    }}
-                >
-                    <span className="sidebar__dot">•</span>
-                    <span className="sidebar__project-name">{project.name}</span>
-                    <span
-                        className="sidebar__project-delete"
-                        onClick={() => console.log('Deleting...')}
-                    >
-                        <FaTrashAlt />
-                    </span>
-                </li>
-            ))
-        );
-    };
+    const [active, setActive] = useState();
+    const { setSelectedProject } = useSelectedProjectValue();
+    const { projects } = useProjectsValue();
+    return (
+        projects &&
+        projects.map(project => (
+            <li
+                key={project.projectId}
+                className={
+                    active === project.projectId
+                        ? 'active sidebar__project'
+                        : 'sidebar__project'
+                }
+                onClick={() => {
+                    setActive(project.projectId);
+                    setSelectedProject(project.projectId);
+                }}
+            >
+                   <IndividualProject project={project} />
+            </li>
+        ))
+    );
+};
