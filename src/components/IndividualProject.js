d3081f88c221515ad70f82ff7ad9246b76663385
gregoryann <32854050+gregoryann@users.noreply.github.com>
2022-07-10 11:51:10 +0100

    test: full test coverage

--- src/components/IndividualProject.js
+++ src/components/IndividualProject.js
@@ -5,7 +5,6 @@ import React, { useState } from 'react';
 import { FaTrashAlt } from 'react-icons/fa';
 import { useProjectsValue, useSelectedProjectValue } from '../context';
 import { firebase } from '../firebase';
-
 export const IndividualProject = ({ project }) => {
     const [showConfirm, setShowConfirm] = useState(false);
     const { projects, setProjects } = useProjectsValue();
@@ -21,31 +20,31 @@ export const IndividualProject = ({ project }) => {
                 setSelectedProject('INBOX');
             });
     };
-
-        return (
-            <>
-                <span className="sidebar__dot">•</span>
-                <span className="sidebar__project-name">{project.name}</span>
-                <span
-                    className="sidebar__project-delete"
-                    onClick={() => setShowConfirm(!showConfirm)}
-                >
-                    <FaTrashAlt />
-                    {showConfirm && (
-                        <div className="project-delete-modal">
-                            <div className="project-delete-modal__inner">
-                                <p>Are you sure you want to delete this project?</p>
-                                    <button
-                                        type="button"
-                                        onClick={() => deleteProject(project.docId)}
-                                    >
-                                        Delete
-                                    </button>
-                                    <span onClick={() => setShowConfirm(!showConfirm)}>Cancel</span>
-                            </div>
+    return (
+        <>
+            <span className="sidebar__dot">•</span>
+            <span className="sidebar__project-name">{project.name}</span>
+            <span
+                className="sidebar__project-delete"
+                data-testid="delete-project"
+                onClick={() => setShowConfirm(!showConfirm)}
+            >
+                <FaTrashAlt />
+                {showConfirm && (
+                    <div className="project-delete-modal">
+                        <div className="project-delete-modal__inner">
+                            <p>Are you sure you want to delete this project?</p>
+                            <button
+                                type="button"
+                                onClick={() => deleteProject(project.docId)}
+                            >
+                                Delete
+                            </button>
+                            <span onClick={() => setShowConfirm(!showConfirm)}>Cancel</span>
                         </div>
-                    )}
-                </span>
-            </>
-        );
-    };
+                    </div>
+                )}
+            </span>
+        </>
+    );
+};
