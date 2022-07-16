227268cb17d57e4d986270d629aa25691beeca7f
gregoryann <32854050+gregoryann@users.noreply.github.com>
2022-07-16 13:38:46 +0100

    feat: tests, refactoring

--- src/App.js
+++ src/App.js
@@ -3,8 +3,10 @@ import PropTypes from 'prop-types';
 import { Header } from './components/layout/Header';
 import { Content } from './components/layout/Content';
 import { ProjectsProvider, SelectedProjectProvider } from './context';
+
 export const App = ({ darkModeDefault = false }) => {
     const [darkMode, setDarkMode] = useState(darkModeDefault);
+
     return (
         <SelectedProjectProvider>
             <ProjectsProvider>
